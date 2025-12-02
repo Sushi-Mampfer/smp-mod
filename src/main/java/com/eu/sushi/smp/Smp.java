7afdbacf192e1223548e9855cd31ef14f8959e47
Sushi-Mampfer <84100259+Sushi-Mampfer@users.noreply.github.com>
2025-12-02 15:50:01 +0100

    Started working on non infinite spawn elytra

--- src/main/java/com/eu/sushi/smp/Smp.java
+++ src/main/java/com/eu/sushi/smp/Smp.java
@@ -5,9 +5,12 @@ import com.eu.sushi.smp.enchantments.SmpEnchantments;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.minecraft.server.MinecraftServer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Smp implements ModInitializer {
     public static final String MOD_ID = "smp";
+    public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
     @Override
     public void onInitialize() {
