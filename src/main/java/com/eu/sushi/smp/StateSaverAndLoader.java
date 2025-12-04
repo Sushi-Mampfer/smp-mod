3a448cbf8c15db88b69384812bd1e5d3ca066956
Sushi-Mampfer <84100259+Sushi-Mampfer@users.noreply.github.com>
2025-12-04 15:56:14 +0100

    Whitelist bot done

--- src/main/java/com/eu/sushi/smp/StateSaverAndLoader.java
+++ src/main/java/com/eu/sushi/smp/StateSaverAndLoader.java
@@ -1,5 +1,6 @@
 package com.eu.sushi.smp;
 
+import com.mojang.datafixers.util.Pair;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import discord4j.common.util.Snowflake;
@@ -12,48 +13,55 @@ import net.minecraft.world.PersistentState;
 import net.minecraft.world.PersistentStateType;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 import static com.eu.sushi.smp.Smp.MOD_ID;
 
 public class StateSaverAndLoader extends PersistentState {
     public HashMap<Snowflake, List<UUID>> snowflake_to_uuids;
     public HashMap<UUID, Snowflake> uuid_to_snowflake;
+    public HashMap<Snowflake, Pair<String, Snowflake>> whitelist_requests;
+
+    private static final Codec<Snowflake> SNOWFLAKE_CODEC = Codec.STRING.xmap(Snowflake::of, Snowflake::asString);
 
     public static final Codec<StateSaverAndLoader> CODEC = RecordCodecBuilder.create(instance ->
         instance.group(
-            Codec.unboundedMap(Uuids.CODEC, Codec.STRING)
+            Codec.unboundedMap(Uuids.CODEC, SNOWFLAKE_CODEC)
                 .fieldOf("whitelists")
-                .forGetter(state ->
-                        state.uuid_to_snowflake.entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey, e -> e.getValue().asString()))
-                )
+                .forGetter(state -> state.uuid_to_snowflake)
+            ,
+            Codec.unboundedMap(SNOWFLAKE_CODEC, Codec.pair(Codec.STRING, SNOWFLAKE_CODEC))
+                    .fieldOf("whitelist_requests")
+                    .forGetter(state -> state.whitelist_requests)
         ).apply(instance, StateSaverAndLoader::new)
     );
 
-    private StateSaverAndLoader(Map<UUID, String> uuidToSnowflakeStrings) {
+    private StateSaverAndLoader(Map<UUID, Snowflake> uuidSnowflakeMap, Map<Snowflake, Pair<String, Snowflake>> snowflakePairMap) {
         this.uuid_to_snowflake = new HashMap<>();
         this.snowflake_to_uuids = new HashMap<>();
+        this.whitelist_requests = new HashMap<>();
 
-        uuidToSnowflakeStrings.forEach((uuid, snowflake) -> {
-            Snowflake snowflake1 = Snowflake.of(snowflake);
-            this.uuid_to_snowflake.put(uuid, snowflake1);
+        uuidSnowflakeMap.forEach((uuid, snowflake) -> {
+            this.uuid_to_snowflake.put(uuid, snowflake);
 
-            if (this.snowflake_to_uuids.containsKey(snowflake1)) {
-                this.snowflake_to_uuids.get(snowflake1).add(uuid);
+            if (this.snowflake_to_uuids.containsKey(snowflake)) {
+                this.snowflake_to_uuids.get(snowflake).add(uuid);
             } else {
-                this.snowflake_to_uuids.put(snowflake1, new ArrayList<>());
-                this.snowflake_to_uuids.get(snowflake1).add(uuid);
+                this.snowflake_to_uuids.put(snowflake, new ArrayList<>());
+                this.snowflake_to_uuids.get(snowflake).add(uuid);
             }
         });
+
+        this.whitelist_requests.putAll(snowflakePairMap);
     }
 
     public StateSaverAndLoader() {
         this.uuid_to_snowflake = new HashMap<>();
         this.snowflake_to_uuids = new HashMap<>();
+        this.whitelist_requests = new HashMap<>();
     }
 
-    private static PersistentStateType<StateSaverAndLoader> TYPE = new PersistentStateType<>(
-            Identifier.of(MOD_ID, "whitelist").toString(),
+    private static final PersistentStateType<StateSaverAndLoader> TYPE = new PersistentStateType<>(
+            MOD_ID,
             StateSaverAndLoader::new,
             CODEC,
             null
@@ -61,5 +69,36 @@ public class StateSaverAndLoader extends PersistentState {
 
     public static StateSaverAndLoader getServerState(MinecraftServer server) {
         return server.getOverworld().getPersistentStateManager().getOrCreate(TYPE);
+    }
+
+    public int whitelistCount(Snowflake snowflake) {
+        List<UUID> list = this.snowflake_to_uuids.get(snowflake);
+        if (list == null) {
+            return 0;
+        }
+        return list.toArray().length;
+    }
+
+    public void addWhitelist(Snowflake snowflake, UUID uuid) {
+        this.uuid_to_snowflake.put(uuid, snowflake);
+        this.snowflake_to_uuids.computeIfAbsent(snowflake, k -> new ArrayList<>());
+        this.snowflake_to_uuids.get(snowflake).add(uuid);
+        this.markDirty();
+    }
+
+    public void removeWhitelist(UUID uuid) {
+        Snowflake snowflake = this.uuid_to_snowflake.remove(uuid);
+        this.snowflake_to_uuids.get(snowflake).remove(uuid);
+        this.markDirty();
+    }
+
+    public void addWhitelistRequest(Snowflake message, String name, Snowflake user) {
+        this.whitelist_requests.put(message, new Pair<>(name, user));
+        this.markDirty();
+    }
+
+    public Pair<String, Snowflake> popWhitelistRequest(Snowflake message) {
+        this.markDirty();
+        return this.whitelist_requests.remove(message);
     }
 }
--- src/main/java/com/eu/sushi/smp/Whitelist.java
+++ src/main/java/com/eu/sushi/smp/Whitelist.java
@@ -1,6 +1,7 @@
 package com.eu.sushi.smp;
 
 
+import com.mojang.datafixers.util.Pair;
 import discord4j.common.util.Snowflake;
 import discord4j.core.DiscordClient;
 import discord4j.core.GatewayDiscordClient;
@@ -8,39 +9,50 @@ import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.event.domain.message.ReactionAddEvent;
 import discord4j.core.object.emoji.Emoji;
 import discord4j.core.object.entity.Message;
-import discord4j.core.spec.MessageCreateMono;
 import discord4j.core.spec.MessageCreateSpec;
 import discord4j.discordjson.json.MessageReferenceData;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.PlayerConfigEntry;
+import net.minecraft.server.PlayerManager;
 import net.minecraft.server.WhitelistEntry;
-import net.minecraft.world.PersistentStateType;
-import net.minecraft.world.World;
 import reactor.core.publisher.Mono;
 
 import java.time.Instant;
-import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
 import static com.eu.sushi.smp.Smp.LOGGER;
 
 public class Whitelist {
+    static private final Snowflake CHANNEL = Snowflake.of("1445523076488888370");
+    static private final Snowflake WHITELIST_PREMIUM = Snowflake.of("1446085376417464413");
+    static private final Snowflake WEBHOOK = Snowflake.of("1446067399618203664");
+
     public static void initialize(MinecraftServer server) {
         Mono<Void> login = DiscordClient.create(System.getenv("TOKEN")).withGateway((GatewayDiscordClient gateway) -> {
             Mono<Void> messageEvent = gateway.on(MessageCreateEvent.class, event ->
                     Mono.fromRunnable(() -> {
                         Message message = event.getMessage();
-                        if (message.getChannelId().asString().equals("1445523076488888370")) {
-                            if (message.getAuthor().get().getGlobalName().get().equals("BingTschiling")) {
-                                LOGGER.info("equal");
+                        if (message.getChannelId().equals(CHANNEL)) {
+                            if (message.getAuthor().isEmpty()) {
+                                return;
+                            }
+                            if (message.getAuthor().get().isBot()) {
+                                return;
+                            }
+                            if (message.getAuthorAsMember().block().getDisplayName().equals("Odoardo")) {
                                 message.getChannel().block().createMessage(MessageCreateSpec.builder()
                                         .content("Du nid")
                                         .messageReference(MessageReferenceData.builder().messageId(message.getId().asLong()).build())
                                         .build()).block();
                                 return;
                             }
-                            LOGGER.info("nequal");
+                            if (message.getContent().equalsIgnoreCase("ghostinator")) {
+                                message.getChannel().block().createMessage(MessageCreateSpec.builder()
+                                        .content("Dä nid")
+                                        .messageReference(MessageReferenceData.builder().messageId(message.getId().asLong()).build())
+                                        .build()).block();
+                                return;
+                            }
                             String content = message.getContent();
                             if (content.equals(content.strip())) {
                                 net.minecraft.server.Whitelist whitelist = server.getPlayerManager().getWhitelist();
@@ -54,12 +66,36 @@ public class Whitelist {
                                     return;
                                 }
                                 StateSaverAndLoader state = StateSaverAndLoader.getServerState(server);
-                                if (newUser(event.getMember().get().getJoinTime().get().getEpochSecond()) || whitelistCount(event.getMember().get().getId(), state) < 1) {
-
+                                if ((newUser(event.getMember().get().getJoinTime().get().getEpochSecond()) || state.whitelistCount(event.getMember().get().getId()) > 0) && event.getMember().get().getRoleIds().contains(WHITELIST_PREMIUM)) {
+                                    message.delete().block();
+                                    Message msg = gateway.getWebhookById(WEBHOOK).flatMap(webhook -> webhook
+                                            .execute().withContent("Please wait for admin verification.\n" + content)
+                                            .withUsername(event.getMember().get().getDisplayName())
+                                            .withAvatarUrl(event.getMember().get().getAvatarUrl())
+                                            .withWaitForMessage(true)
+                                    ).block();
+                                    if (msg == null) {
+                                        return;
+                                    }
+                                    msg.addReaction(Emoji.unicode("✅")).block();
+                                    msg.addReaction(Emoji.unicode("❌")).block();
+                                    state.addWhitelistRequest(msg.getId(), content, event.getMember().get().getId());
+                                    return;
+                                }
+                                message.delete().block();
+                                Message msg = gateway.getWebhookById(WEBHOOK).flatMap(webhook -> webhook
+                                        .execute().withContent(content)
+                                        .withUsername(event.getMember().get().getDisplayName())
+                                        .withAvatarUrl(event.getMember().get().getAvatarUrl())
+                                        .withWaitForMessage(true)
+                                ).block();
+                                if (msg == null) {
+                                    return;
                                 }
                                 WhitelistEntry entry = new WhitelistEntry(player.get());
+                                state.addWhitelist(event.getMember().get().getId(), player.get().id());
                                 whitelist.add(entry);
-                                message.addReaction(Emoji.unicode("✅")).block();
+                                msg.addReaction(Emoji.unicode("✅")).block();
                             } else {
                                 message.delete().block();
                             }
@@ -68,7 +104,56 @@ public class Whitelist {
             ).then();
             Mono<Void> reactEvent = gateway.on(ReactionAddEvent.class, event ->
                     Mono.fromRunnable(() -> {
+                        if (event.getChannelId().equals(CHANNEL)) {
+                            Message message = event.getMessage().block();
+                            Emoji emoji = event.getEmoji();
+                            StateSaverAndLoader state = StateSaverAndLoader.getServerState(server);
+                            if (emoji.equals(Emoji.unicode("✅"))) {
+                                if (event.getMember().get().getRoleIds().contains(WHITELIST_PREMIUM)) {
+                                    Pair<String, Snowflake> pair = state.popWhitelistRequest(event.getMessageId());
+                                    if (pair == null) {
+                                        message.delete();
+                                        return;
+                                    }
+                                    net.minecraft.server.Whitelist whitelist = server.getPlayerManager().getWhitelist();
+                                    Optional<PlayerConfigEntry> player = server.getApiServices().nameToIdCache().findByName(pair.getFirst());
+                                    if (player.isEmpty()) {
+                                        message.delete().block();
+                                        return;
+                                    }
+                                    if (whitelist.isAllowed(player.get())) {
+                                        message.delete().block();
+                                        return;
+                                    }
+                                    state.addWhitelist(pair.getSecond(), player.get().id());
+                                    message.removeAllReactions().block();
+                                    message.addReaction(Emoji.unicode("✅")).block();
+                                    gateway.getWebhookById(WEBHOOK).flatMap(webhook -> webhook
+                                            .editMessage(event.getMessageId())
+                                            .withContent(pair.getFirst())
+                                    ).block();
+                                }
+                            } else if (event.getEmoji().equals(Emoji.unicode("❌"))) {
+                                if (event.getMember().get().getRoleIds().contains(WHITELIST_PREMIUM)) {
+                                    String content = message.getContent();
+                                    message.delete().block();
+                                    if (state.popWhitelistRequest(event.getMessageId()) != null) {
+                                        message.delete().block();
+                                        return;
+                                    }
+                                    Optional<PlayerConfigEntry> player = server.getApiServices().nameToIdCache().findByName(content);
+                                    if (player.isEmpty()) {
+                                        return;
+                                    }
+                                    PlayerManager playerManager = server.getPlayerManager();
+                                    playerManager.remove(playerManager.getPlayer(player.get().id()));
+                                    net.minecraft.server.Whitelist whitelist = playerManager.getWhitelist();
+                                    whitelist.remove(player.get());
+                                    state.removeWhitelist(player.get().id());
 
+                                }
+                            }
+                        }
                     })).then();
             return messageEvent.and(reactEvent);
         });
@@ -77,13 +162,5 @@ public class Whitelist {
 
     private static boolean newUser(long timestamp) {
         return Instant.now().getEpochSecond() - timestamp < 24 * 60 * 60 * 30;
-    }
-
-    private static int whitelistCount(Snowflake snowflake, StateSaverAndLoader state) {
-        List<UUID> list = state.snowflake_to_uuids.get(snowflake);
-        if (list == null) {
-            return 0;
-        }
-        return list.toArray().length;
     }
 }
